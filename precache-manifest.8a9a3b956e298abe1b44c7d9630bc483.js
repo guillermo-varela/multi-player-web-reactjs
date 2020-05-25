16ad22706bd9e04635a724e4b4780e9c7b121f85
Guillermo Varela <gvarela@sonatype.com>
2020-05-25 01:29:06 -0500

    Updates

---
+++ precache-manifest.8a9a3b956e298abe1b44c7d9630bc483.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9ea61f2b5aa376823cfd4f81301e1f2",
+    "url": "/multi-player-web-reactjs/index.html"
+  },
+  {
+    "revision": "dbc5f11706caa65c020b",
+    "url": "/multi-player-web-reactjs/static/css/4.89f067aa.chunk.css"
+  },
+  {
+    "revision": "f318e5071f80c387471f",
+    "url": "/multi-player-web-reactjs/static/css/5.85c405f7.chunk.css"
+  },
+  {
+    "revision": "428c0e55161283259558",
+    "url": "/multi-player-web-reactjs/static/css/6.ccf72c0a.chunk.css"
+  },
+  {
+    "revision": "58daa16ccaf60eb4960d",
+    "url": "/multi-player-web-reactjs/static/css/7.14fc1339.chunk.css"
+  },
+  {
+    "revision": "b6de19bf1ac2a0d5a9da",
+    "url": "/multi-player-web-reactjs/static/css/main.0d98cd0d.chunk.css"
+  },
+  {
+    "revision": "52f00cab8fb465e7b05c",
+    "url": "/multi-player-web-reactjs/static/js/0.ee94f7e0.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/multi-player-web-reactjs/static/js/0.ee94f7e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78c0f8755afba60622b6",
+    "url": "/multi-player-web-reactjs/static/js/1.23dd0cd1.chunk.js"
+  },
+  {
+    "revision": "bd49b04565accd5b6f2624c86f8385ba",
+    "url": "/multi-player-web-reactjs/static/js/1.23dd0cd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbc5f11706caa65c020b",
+    "url": "/multi-player-web-reactjs/static/js/4.5fc2ba33.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/multi-player-web-reactjs/static/js/4.5fc2ba33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f318e5071f80c387471f",
+    "url": "/multi-player-web-reactjs/static/js/5.12e65824.chunk.js"
+  },
+  {
+    "revision": "428c0e55161283259558",
+    "url": "/multi-player-web-reactjs/static/js/6.740f9310.chunk.js"
+  },
+  {
+    "revision": "58daa16ccaf60eb4960d",
+    "url": "/multi-player-web-reactjs/static/js/7.b0742292.chunk.js"
+  },
+  {
+    "revision": "b6de19bf1ac2a0d5a9da",
+    "url": "/multi-player-web-reactjs/static/js/main.878e1869.chunk.js"
+  },
+  {
+    "revision": "99a106fc711a3abe0b79",
+    "url": "/multi-player-web-reactjs/static/js/runtime-main.06c4313e.js"
+  }
+]);
